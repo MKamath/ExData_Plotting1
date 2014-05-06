575777991011604462af175c0cc1714841b36d0f
MKamath <kamathmridula@yahoo.co.in>
2014-05-06 20:58:09 +0530

    Version 2.1

--- Plot1.R
+++ Plot1.R
@@ -7,3 +7,4 @@ sub<- subset(Data, (Data$Date == "1/2/2007" | Data$Date== "2/2/2007"))
 # creating plot1
 png("plot1.png", width=480, height= 480)
 hist(sub$Global_active_power, col= "red", xlab= "Global Active Power (kilowatts)", ylab= "Frequency", main= "Global Active Power")
+dev.off()
--- Plot2.R
+++ Plot2.R
@@ -9,3 +9,4 @@ sub$DateTime <- as.POSIXct(paste(sub$Date, sub$Time))
 # Creating the plot2:
 png("plot2.png", width=480, height= 480)
 plot(sub$DateTime, sub$Global_active_power, type= "l", lwd=1, ylab= "Global Active Power (kilowatts)", xlab="")
+dev.off()
